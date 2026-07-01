38b507b8006ff9e2d0be96a078182277e4a0db9a
zombiegirlcz <255570335+zombiegirlcz@users.noreply.github.com>
2026-07-01 06:30:03 +0000

    security audiť fix

--- tools/aapt2-wrapper.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Wrapper to run x86_64 aapt2 via qemu-user on arm64
-export LD_LIBRARY_PATH=/tmp/libc6_extract/usr/lib/x86_64-linux-gnu
-exec qemu-x86_64 "$ANDROID_HOME/build-tools/36.0.0/aapt2" "$@"
