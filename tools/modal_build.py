51717aec6b0d43b2cb333c0d0d7acc73bd598155
zombiegirlcz <zombiegirlcz@gmail.com>
2026-09-06 10:37:39 +0000

    feat: install git-lfs in Modal base image

--- tools/modal_build.py
+++ tools/modal_build.py
@@ -70,7 +70,7 @@ NDK_DIR = f"/opt/android-ndk-{NDK_VERSION}"
 
 base_image = (
     modal.Image.from_registry("eclipse-temurin:21-jdk")
-    .apt_install("unzip", "wget", "git", "file", "rsync", "python3", "python3-pip", "python-is-python3",
+    .apt_install("unzip", "wget", "git", "git-lfs", "file", "rsync", "python3", "python3-pip", "python-is-python3",
                   "bison", "flex", "cmake", "make", "ninja-build", "pkg-config", "libssl-dev", "build-essential")
     .run_commands(
         "mkdir -p /opt/android-sdk/cmdline-tools",
