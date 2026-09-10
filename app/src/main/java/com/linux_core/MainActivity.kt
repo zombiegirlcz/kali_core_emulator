4f09846308f5d8c04a4fec8b8550a3a1752306ef
zombiegirlcz <zombiegirlcz@gmail.com>
2026-09-10 05:39:22 +0200

    fix: restore mutableStateOf import alongside mutableStateMapOf

--- app/src/main/java/com/linux_core/MainActivity.kt
+++ app/src/main/java/com/linux_core/MainActivity.kt
@@ -74,6 +74,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
